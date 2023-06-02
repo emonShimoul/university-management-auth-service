b19f7d8a88dc2ef2da463aa4c10ec53d607d2daa
emonShimoul <emonshimoul@gmail.com>
2023-06-02 20:37:02 +0600

    [DONE] setup the winston logger library

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
@@ -10,6 +10,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // application routes
+// console.log(process.env)
+
 app.use('/api/v1/users/', usersRouter)
 
 // Testing
---
+++ src/shared/logger.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+import winston, { createLogger } from 'winston'
+
+const logger = createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
