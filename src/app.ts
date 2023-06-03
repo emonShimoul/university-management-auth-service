1201a2e42fcb5c468b8b6db2a5c15cf4294740ac
emonShimoul <emonshimoul@gmail.com>
2023-06-03 15:38:14 +0600

    [DONE] Optimize error handling methodology

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
@@ -15,8 +16,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/users/', usersRouter)
 
 // Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully!!')
-})
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   // res.send('Working Successfully!!')
+//   throw new Error('Custom Error')
+//   // next('Customm Errorrr') // Error
+// })
+
+// global error handler
+app.use(globalErrorHandler)
 
 export default app
