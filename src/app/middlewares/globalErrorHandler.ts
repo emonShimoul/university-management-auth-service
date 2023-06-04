92f42fab9c737ed66aaa6e0c16399cee0244c8f2
emonShimoul <emonshimoul@gmail.com>
2023-06-04 15:56:06 +0600

    refactor global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import config from '../../config'
+import ApiError from '../../errors/ApiError'
+import handleValidationError from '../../errors/handleValidationError'
 import { IGenericErrorMessage } from '../../interfaces/error'
 
 const globalErrorHandler = (
@@ -8,9 +10,37 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode = 500
-  const message = 'Something went wrong!!'
-  const errorMessages: IGenericErrorMessage[] = []
+  let statusCode = 500
+  let message = 'Something went wrong!!'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
